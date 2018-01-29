47fb012be385b33299764e272651fa14bb45ff7c
Kenta Mukai <k_mukai@vivivit.co.jp>
2018-01-29 16:44:09 +0900

    :sparkles: post to slack

--- src/Code.ts
+++ src/Code.ts
@@ -2,5 +2,21 @@ function myFunction() {
   const url = PropertiesService.getScriptProperties().getProperty('webhook_url');
   if (url) {
     Logger.log(url);
+    postSlack(url);
   }
+}
+
+function postSlack(url: string) {
+  const jsonData = {
+    username: 'bot-x',
+    icon_emoji: ':chicken:',
+    text: 'test',
+  }
+  const payload = JSON.stringify(jsonData);
+  const options = {
+    method: 'post' as 'post', // こう書くとなぜかtypescriptのcheckを通る
+    contentType: 'application/json',
+    payload: payload,
+  }
+  UrlFetchApp.fetch(url, options);
 }
